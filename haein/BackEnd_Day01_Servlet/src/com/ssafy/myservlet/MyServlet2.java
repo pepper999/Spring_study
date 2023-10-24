647d6db01901164b8105f94681988afc1a738e5f
김해인 <khi762@naver.com>
2023-10-24 18:00:07 +0900

    servlet

---
+++ haein/BackEnd_Day01_Servlet/src/com/ssafy/myservlet/MyServlet2.java
@@ -0,0 +1,23 @@
+package com.ssafy.myservlet;
+
+import java.io.IOException;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+// 해당 클래스를 서블릿으로 만들기 위해서는 Servlet을 구현해야한다.
+public class MyServlet2 extends GenericServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+}
---
+++ haein/BackEnd_Day01_Servlet/src/com/ssafy/myservlet/MyServlet3.java
@@ -0,0 +1,11 @@
+package com.ssafy.myservlet;
+
+import javax.servlet.http.HttpServlet;
+
+// 해당 클래스를 서블릿으로 만들기 위해서는 Servlet을 구현해야한다.
+public class MyServlet3 extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	
+	
+}
